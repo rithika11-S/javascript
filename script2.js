8932f84e49da83f618aef41749231f87c9f1fc6a
rithika11-S <kit27.cse44@gmail.com>
2026-04-30 13:19:30 +0530

    Day 4 work

---
+++ script2.js
@@ -0,0 +1,57 @@
+// const a=[1,2,3,4]
+// console.log(a.at(2))
+
+// const b=[1,2,3,4]
+// const c=[5,6,7]
+// console.log(b.concat(c))
+
+// const d=[1,2,3,4]
+// console.log(d.copyWithin(1,3))
+// console.log(d)
+
+// const e=[1,2,3,4]
+// const f=e.entries()
+// console.log(f.next().value)
+// console.log(f.next().value)
+// for (let value of f){
+//     console.log(value);
+// }
+
+// const g=[2,4,6,8]
+// const h=g.every((n)=>n%2==0) 
+// console.log(h)
+
+// const i=[1,2,3,4]
+// console.log(i.fill(0,2))
+// console.log(i)
+
+// const j=[1,2,3,4]
+// const k=j.filter((n)=>n%2==0)
+// console.log(k)
+
+// const l=[1,2,3,4]
+// const m=l.find((n)=>n>1)
+// const m1=l.findLast((n)=>n>1)
+// console.log(m)
+// console.log(m1)
+
+
+// const n=[1,2,3,4]
+// const o=n.findIndex((n)=>n>1)
+// const o1=n.findLastIndex((n)=>n+1)
+// console.log(o)
+// console.log(o1)
+
+// const p=[1,2,3,[4,5]]
+// console.log(p.flat())
+// console.log(p)
+
+// const q=[1,2,3,4]
+// const r=q.forEach((n)=>{
+//     console.log(n)
+// })
+
+
+// const s=[1,2,3,4]
+// console.log(s.includes(4))
+
