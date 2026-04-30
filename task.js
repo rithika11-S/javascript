d08483a5ff034c3b84cf409606ae82ffc3025fd5
rithika11-S <kit27.cse44@gmail.com>
2026-04-30 11:39:25 +0530

    first commit

---
+++ task.js
@@ -0,0 +1,42 @@
+// 1 
+const obj={
+    msg:"Hello World",
+}
+console.log((obj.msg.toUpperCase()).includes("HELLO"));
+
+
+
+// 2
+
+
+const num={
+    numb:80,
+}
+console.log(Math.pow(num.numb,2)>400);
+
+// 3 
+
+const addi={
+    num1:10,
+    num2:15,
+}
+console.log((addi.num1+addi.num2)>150);
+
+// 4
+
+const count={
+    n1:100,
+    n2:200,
+    n3:300,
+    n4:500
+}
+console.log((Math.max(count.n1,count.n2,count.n3,count.n4))>400)
+
+// 5
+
+const len={
+    sen:"I am happy to be part of this gathering"
+}
+console.log((len.sen.length)>30)
+
+
